5acace160229487cfc9a92bab69dcae69510ee08
Russell <russell@russelldunphy.com>
2016-04-11 16:50:19 +0100

    bricks

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(function(){
   $('.scroll-link').on('click', function(e){
     console.log(e);
     var section = $(this).attr("href");
-    $('html,body').animate({scrollTop: ($(section).offset().top - 50)}, 1000);
+    $('html,body').animate({scrollTop: ($(section).offset().top - 180)}, 1000);
     e.preventDefault();
   });
 });
