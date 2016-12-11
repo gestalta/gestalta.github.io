7d151395e0941c3688cfcb060707f6efac927433
gestalta <gestalta22@gmail.com>
2016-12-11 15:49:30 +0000

    some stuff I deleted

--- subs.sh
+++
@@ -1,2 +0,0 @@
-find . -name '*.md' | xargs sed -i '' 's/ shibari\/performance\/2015-11-paris-gestalta-gorgone\/paris-gestalta-gorgone40\.jpg/ https:\/\/farm8\.staticflickr\.com\/7450\/27016812334_550881f45f_o\.jpg/g'
-find . -name '*.md' | xargs sed -i '' 's/ shibari\/photos\/2016-02-sophia\/gestalta-sophia7\.jpg/ https:\/\/farm8\.staticflickr\.com\/7338\/27349494200_38131747bf_o\.jpg/g'
