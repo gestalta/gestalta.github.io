06fe765f729e5f6df9920c3f86dfcb18d4cf2b56
gestalta <gestalta22@gmail.com>
2016-12-19 16:08:30 +0000

    wip

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,18 @@
 (function($) {
-  $(function() {
-    $('nav ul li > a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').toggle();
-      $('.nav-dropdown').not($(this).siblings()).hide();
-      e.stopPropagation();
-    });
-    $('html').click(function() {
-      $('.nav-dropdown').hide();
-    });
+
+  $('.submenu a').click(function(e) {
+    $('.submenu').hide();
+    e.stopPropagation();
   });
+
+  $('html').click(function() {
+    $('.nav-dropdown').hide();
+  });
+
   $('#nav-toggle').click(function(e) {
-    $(e).toggleClass('active');
+    console.log(e.target);
+    $(e.target).toggleClass('active');
     $('.submenu').toggle();
   });
+
 })(jQuery);
