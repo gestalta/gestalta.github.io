2987423517cc432e0831862b3edbbb3c7498aac0
gestalta <gestalta22@gmail.com>
2016-12-19 11:05:54 +0000

    fixing bugs

--- js/nav.js
+++ js/nav.js
@@ -9,10 +9,8 @@
       $('.nav-dropdown').hide();
     });
   });
-  document.querySelector('#nav-toggle').addEventListener('click', function() {
-    this.classList.toggle('active');
-  });
-  $('#nav-toggle').click(function() {
+  $('#nav-toggle').click(function(e) {
+    $(e).toggleClass('active');
     $('nav ul').toggle();
   });
 })(jQuery);
